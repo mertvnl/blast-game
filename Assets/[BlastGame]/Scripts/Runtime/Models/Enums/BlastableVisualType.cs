2db6c04f3064eb355be1935cbd211e5c805a71e2
Mert Vanlıoğlu <mertvanlioglu1337@gmail.com>
2023-09-01 05:52:58 +0300

    Create BlastableVisualType.cs

---
+++ Assets/[BlastGame]/Scripts/Runtime/Models/Enums/BlastableVisualType.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BlastGame.Runtime.Models
+{
+    public enum BlastableVisualType
+    {
+        Default = 0,
+        A = 10,
+        B = 20,
+        C = 30
+    }
+}
