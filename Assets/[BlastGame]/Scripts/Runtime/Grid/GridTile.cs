1d2944ca084111c9d13367d993806678c34f57bd
Mert Vanlıoğlu <mertvanlioglu1337@gmail.com>
2023-09-01 05:52:07 +0300

    Update GridTile.cs

--- Assets/[BlastGame]/Scripts/Runtime/Grid/GridTile.cs
+++ Assets/[BlastGame]/Scripts/Runtime/Grid/GridTile.cs
@@ -1,3 +1,4 @@
+using BlastGame.Interface;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,31 @@ namespace BlastGame.Runtime
 {
     public class GridTile : MonoBehaviour
     {
+        private int _x;
+        public int X => _x;
+        private int _y;
+        public int Y => _y;
 
+        public bool IsEmpty => CurrentItem != null;
+
+        public IItem CurrentItem { get; private set; }
+
+        public void InitializeTile(int x, int y)
+        {
+            _x = x;
+            _y = y;
+
+            gameObject.name = "[" + x + "," + y + "]";
+        }
+
+        public void SetItem(IItem item)
+        {
+            CurrentItem = item;
+        }
+
+        public Vector2 GetPosition()
+        {
+            return new Vector2(_x, _y);
+        }
     }
 }
