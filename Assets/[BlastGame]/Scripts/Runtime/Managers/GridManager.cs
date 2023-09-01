f5a8b28537b74525905bc9351c9d83b18c6731ce
Mert Vanlıoğlu <mertvanlioglu1337@gmail.com>
2023-09-01 05:52:37 +0300

    Update GridManager.cs

--- Assets/[BlastGame]/Scripts/Runtime/Managers/GridManager.cs
+++ Assets/[BlastGame]/Scripts/Runtime/Managers/GridManager.cs
@@ -1,3 +1,6 @@
+using BlastGame.Interface;
+using Core.Managers;
+using Core.Systems;
 using Core.Utilities;
 using System.Collections;
 using System.Collections.Generic;
@@ -7,6 +10,51 @@ namespace BlastGame.Runtime
 {
     public class GridManager : Singleton<GridManager>
     {
+        [SerializeField] private int width, height;
+        [SerializeField] private GridTile gridTilePrefab;
+        
+        private const float X_OFFSET = -0.15f;
+        private const float Y_OFFSET = 1f;
 
+        public CustomEvent<int, int> OnGridInitialized = new();
+
+        private Dictionary<Vector2, GridTile> _tiles;
+        public Dictionary<Vector2, GridTile> Tiles => _tiles;
+
+        private void OnEnable()
+        {
+            LevelManager.Instance.OnLevelLoaded.AddListener(InitializeGrid);
+        }
+
+        private void OnDisable()
+        {
+            LevelManager.Instance.OnLevelLoaded.RemoveListener(InitializeGrid);
+        }
+
+        public void InitializeGrid()
+        {
+            _tiles = new();
+            Transform gridRoot = new GameObject("GridRoot").transform;
+
+            for (int x = 0; x < width; x++)
+            {
+                for(int y = 0; y < height; y++)
+                {
+                    GridTile instantiatedTile = Instantiate(gridTilePrefab, new Vector3(x, y), Quaternion.identity, gridRoot);
+                    instantiatedTile.InitializeTile(x, y);
+                    _tiles.Add(new Vector2(x,y), instantiatedTile);
+                }
+            }
+
+            OnGridInitialized.Invoke(width, height);
+        }
+
+        public GridTile GetTileAtPosition(Vector2 position)
+        {
+            if (_tiles.TryGetValue(position, out GridTile tile))
+                return tile;
+
+            return null;
+        }
     }
 }
