a745283d0529f9baeabee63b8f8ee8d15fc2cb1f
Mert Vanlıoğlu <mertvanlioglu1337@gmail.com>
2023-09-02 04:01:59 +0300

    Update GridManager.cs

--- Assets/[BlastGame]/Scripts/Runtime/Managers/GridManager.cs
+++ Assets/[BlastGame]/Scripts/Runtime/Managers/GridManager.cs
@@ -74,5 +74,50 @@ namespace BlastGame.Runtime
 
             return null;
         }
+
+        public List<GridTile> GetEmptyTiles()
+        {
+            List<GridTile> emptyTiles = new();
+
+            for (int x = width - 1; x >= 0; x--)
+            {
+                for (int y = height - 1; y >= 0; y--)
+                {
+                    GridTile tile = GetTileAtPosition(new Vector2(x, y));
+
+                    if (!tile.IsEmpty)
+                        continue;
+
+                    emptyTiles.Add(tile);
+                }
+            }
+
+            return emptyTiles;
+        }
+
+        public List<GridTile> GetTilesWithItem()
+        {
+            List<GridTile> tilesWithItem = new();
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    GridTile tile = GetTileAtPosition(new Vector2(x, y));
+
+                    if (tile.IsEmpty)
+                        continue;
+
+                    tilesWithItem.Add(tile);
+                }
+            }
+
+            return tilesWithItem;
+        }
+
+        public Vector2 GetGridSize()
+        {
+            return new Vector2(width, height);
+        }
     }
 }
